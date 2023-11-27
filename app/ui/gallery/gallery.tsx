cbab16c1519542b2247892137dd5b16d9ada0553
Roddy <adarkwagodred7075@icloud.com>
2023-11-27 15:00:24 +0000

    gallery bug fixed

--- app/ui/gallery/gallery.tsx
+++ app/ui/gallery/gallery.tsx
@@ -5,11 +5,11 @@ export default function Gallery() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center pb-20">
       <div
-        className="w-[94%] md:w-[60%] h-[85vh] md:h-[72rem]  rounded-[20px] relative
+        className="w-[94%] md:w-[60%] h-[90vh] md:h-[72rem]  rounded-[20px] relative
          flex justify-end overflow-hidden  mt-[10rem] md:mt-[12rem]" id="gallery" >
         <div className="w-full h-full absolute flex justify-between">
           <div className="w-[65%] h-full bg-gradient-to-b from-white_blue to-cream_blue">
-            <div className="w-[80%] z-50 cursor-pointer relative p-5 px-4 max-sm:px-2 flex justify-center items-center">
+            <div className="md:w-[80%] w-[85%] z-50 cursor-pointer relative p-5 px-4 max-sm:px-2 flex justify-center items-center">
               <Text />
             </div>
           </div>
